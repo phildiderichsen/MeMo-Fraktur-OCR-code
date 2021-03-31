fea6e1a13838f755ee3e7a5c9d167b3d52317594
Philip Diderichsen <philip.diderichsen@gmail.com>
2021-03-31 12:08:07 +0200

    Handle cross-page hyphens in pages2vrt.

--- memoocr/pages2vrt.py
+++ memoocr/pages2vrt.py
@@ -20,9 +20,10 @@ def main():
     config = configparser.ConfigParser()
     config.read(os.path.join(os.path.dirname(__file__), '..', 'config', 'config.ini'))
     conf = config['DEFAULT']
-    testfolder = os.path.join(conf['intermediatedir'], 'corr_pages', '1870_Lange_AaenOgHavet')
+    testfolder = os.path.join(conf['intermediatedir'], 'corr_pages', '1871_MauT_Deodata')
 
-    pages2vrt(testfolder)
+    vrt = pages2vrt(testfolder)
+    print(vrt)
 
     endtime = datetime.now()
     elapsed = endtime - starttime
@@ -32,7 +33,7 @@ def main():
 
 
 def pages2vrt(pagedir):
-    """Convert pages to VRT file."""
+    """Convert pages to text represented in VRT format."""
 
     def get_pagenum(page: str):
         """Extract page number from page filename."""
@@ -40,32 +41,52 @@ def pages2vrt(pagedir):
 
     pages = [os.path.join(pagedir, p) for p in os.listdir(pagedir)]
     pagenums = [int(get_pagenum(p)) for p in os.listdir(pagedir)]
-    vrt_pages = [page2vrt(page, pagenum) for page, pagenum in zip(pages, pagenums)]
-    # TODO: When concatenating pages, eliminate hyphenation at end of page.
-    print('<text id="{}">\n{}\n</text>'.format(pagedir, "\n".join(vrt_pages)))
+    tokenlists = [page2tokens(page, pagenum) for page, pagenum in zip(pages, pagenums)]
+    texttokens = flatten_tokenlists(tokenlists)
+    vrt_lines = [f'{d["token"]}\t{d["i"]}\t{d["line"]}\t{d["page"]}' for d in texttokens]
+    vrt_text = '<text id="{}">\n{}\n</text>'.format(pagedir, "\n".join(vrt_lines))
+    return vrt_text
 
 
-def page2vrt(page, pagenum):
-    """Transform a single page to VRT format."""
+def page2tokens(page, pagenum):
+    """Transform a single page to a list of dicts each containing a token and some annotations."""
+
+    def handle_hyphens(text):
+        """Eliminate end of line hyphens."""
+        return re.sub(r'(\S+)[⸗—-]\n(\S+) ', r'\1\2\n', text)
+
+    def make_line_tokens(line: str, linenum: int, _pagenum: int):
+        """Tokenize a line and enumerate the tokens by number on line, line number, and page number."""
+        tokens = word_tokenize(line, language='danish')
+        return [{'token': tok, 'i': i + 1, 'line': linenum, 'page': _pagenum} for i, tok in enumerate(tokens)]
+
     with open(page, 'r') as infile:
         pagetext = infile.read()
     pagetext = handle_hyphens(pagetext)
-    pagelines = [process_line(l, linenum+1, pagenum) for linenum, l in enumerate(pagetext.splitlines())]
+    pagelines = [make_line_tokens(line, linenum+1, pagenum) for linenum, line in enumerate(pagetext.splitlines())]
     pagetokens = [tokendict for sublist in pagelines for tokendict in sublist]
-    vrt_lines = [f'{d["token"]}\t{d["i"]}\t{d["line"]}\t{d["page"]}' for d in pagetokens]
-    return '\n'.join(vrt_lines)
+    return pagetokens
 
 
-def handle_hyphens(text):
-    """Eliminate end of line hyphens."""
-    return re.sub(r'(\S+)[⸗—-]\n(\S+) ', r'\1\2\n', text)
+def flatten_tokenlists(tokenlists: list):
+    """Flatten lists of page tokens to one big list of text tokens. Eliminate cross-page hyphens."""
 
+    def handle_cross_page_hyphen(page_toks: list, next_page_toks: list):
+        """Eliminate hyphenation between page and next_page, if any."""
+        if re.match(r'\S+[⸗—-]', page_toks[-1]["token"]) and re.match(r'\S+', next_page_toks[0]["token"]):
+            new_prefix = re.sub(r'(\S+)[⸗—-]', r'\1', page_toks[-1]["token"])
+            page_toks[-1]["token"] = new_prefix + next_page_toks[0]["token"]
+            next_page_toks = next_page_toks[1:]
+        return page_toks, next_page_toks
 
-def process_line(line: str, linenum: int, pagenum: int):
-    """Tokenize a line and enumerate the tokens by number on line, line number, and page number."""
-    tokens = word_tokenize(line, language='danish')
-    return [{'token': tok, 'i': i+1, 'line': linenum, 'page': pagenum} for i, tok in enumerate(tokens)]
-
+    # For each page: a tuple of the index of the page and the index of the next page (if any)
+    indexes = list(range(len(tokenlists)))
+    indextuples = list(zip(indexes, indexes[1:]))
+    for indextup in indextuples:
+        i, j = indextup
+        tokenlists[i], tokenlists[j] = handle_cross_page_hyphen(tokenlists[i], tokenlists[j])
+    texttokens = [tokendict for sublist in tokenlists for tokendict in sublist]
+    return texttokens
 
 
 if __name__ == '__main__':
