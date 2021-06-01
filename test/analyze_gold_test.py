6bfec0b841b0e0d32a21f5015be03f8479faced1
Philip Diderichsen <philip.diderichsen@gmail.com>
2021-06-01 17:03:10 +0200

    Remove test module. I never use it.

--- test/analyze_gold_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class GoldDataTest(unittest.TestCase):
-    """Test the configuration option handling."""
-
-    def test_no_empty_lines(self):
-        self.assertTrue(os.path.exists(pathresult))
